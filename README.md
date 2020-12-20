c413dab5a1ce41a8274809f003177558b5e569b2
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2020-12-20 21:38:13 +0100

    tests : no leaks

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # FSM
 Small implementation of Finite State Machines in C
+gcc int_array.c uchar_array.c transition_array.c nfa.c test_nfa.c -std=c99
--- test_nfa.c
+++ test_nfa.c
@@ -2,10 +2,24 @@
 #include<stdlib.h>
 
 void test_create_empty_language() {
-    create_empty_language();
+    struct nfa a;
+
+    a = create_empty_language();
+
+    free_nfa(&a);
+}
+
+void test_create_empty_string_language() {
+    struct nfa a;
+
+    a = create_empty_string_language();
+
+    free_nfa(&a);
 }
 
 int main() {
     test_create_empty_language();
+    test_create_empty_string_language();
+
     return EXIT_SUCCESS;
 }
